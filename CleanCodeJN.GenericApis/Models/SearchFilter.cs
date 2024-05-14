c2981e50d649c3b49d00173da3cd39b06745c29b
Jörg Nieveler <joerg.nieveler@live.de>
2024-05-14 12:02:05 +0200

    New Version 2.0.7.: Add ToJson() to SearchFilter.

--- CleanCodeJN.GenericApis/Models/SearchFilter.cs
+++ CleanCodeJN.GenericApis/Models/SearchFilter.cs
@@ -1,6 +1,10 @@
-﻿namespace CleanCodeJN.GenericApis.Models;
+﻿using System.Text.Json;
+
+namespace CleanCodeJN.GenericApis.Models;
 
 public class SearchFilter
 {
     public List<FilterValue> Filters { get; set; }
+
+    public string ToJson() => JsonSerializer.Serialize(this);
 }
