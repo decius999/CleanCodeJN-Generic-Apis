908f8926f643f779e82bb8170d8db7cea4dff720
Jörg Nieveler <joerg.nieveler@live.de>
2024-05-22 10:05:13 +0200

    New Version 2.1.1: Support for global Exception Handlers.

--- CleanCodeJN.GenericApis/API/ApiBase.cs
+++ CleanCodeJN.GenericApis/API/ApiBase.cs
@@ -19,63 +19,30 @@ public class ApiBase : ControllerBase
 
     public async Task<IResult> HandlePagination<TEntity, TDto>(IRequest<BaseListResponse<TEntity>> request)
     {
-        try
-        {
-            var domainResult = await CommandBus.Send(request);
+        var domainResult = await CommandBus.Send(request);
 
-            var dtos = Mapper.Map<List<TDto>>(domainResult.Data);
-            var response = await BaseListResponse<TDto>.Create(domainResult.ResultState, dtos, domainResult.Message, domainResult.Count);
+        var dtos = Mapper.Map<List<TDto>>(domainResult.Data);
+        var response = await BaseListResponse<TDto>.Create(domainResult.ResultState, dtos, domainResult.Message, domainResult.Count);
 
-            return domainResult.AsHttpResult(response);
-        }
-        catch (Exception e)
-        {
-            return Results.Problem(title: e.Message, detail: e.StackTrace, statusCode: 500);
-        }
+        return domainResult.AsHttpResult(response);
     }
 
     public async Task<IResult> Handle<TEntity, TDto>(IRequest<BaseListResponse<TEntity>> request, Func<BaseListResponse<TEntity>, TDto> map = null)
     {
-        try
-        {
-            var domainResult = await CommandBus.Send(request);
+        var domainResult = await CommandBus.Send(request);
 
-            return domainResult.AsHttpResult(map == null ? Mapper.Map<TDto>(domainResult.Data) : map(domainResult));
-        }
-        catch (Exception e)
-        {
-            return Results.Problem(title: e.Message, detail: e.StackTrace, statusCode: 500);
-        }
+        return domainResult.AsHttpResult(map == null ? Mapper.Map<TDto>(domainResult.Data) : map(domainResult));
     }
 
     public async Task<IResult> Handle<TEntity, TDto>(IRequest<BaseResponse<TEntity>> request, Func<BaseResponse<TEntity>, TDto> map = null)
         where TEntity : class
     {
-        try
-        {
-            var domainResult = await CommandBus.Send(request);
+        var domainResult = await CommandBus.Send(request);
 
-            return domainResult.AsHttpResult(map == null ? Mapper.Map<TDto>(domainResult.Data) : map(domainResult));
-        }
-        catch (Exception e)
-        {
-            return Results.Problem(title: e.Message, detail: e.StackTrace, statusCode: 500);
-        }
+        return domainResult.AsHttpResult(map == null ? Mapper.Map<TDto>(domainResult.Data) : map(domainResult));
     }
 
     public async Task<IResult> Handle<TEntity>(IRequest<Response> request)
-        where TEntity : class
-    {
-        try
-        {
-            var domainResult = await CommandBus.Send(request);
-
-            return domainResult.AsHttpResult();
-        }
-        catch (Exception e)
-        {
-            return Results.Problem(title: e.Message, detail: e.StackTrace, statusCode: 500);
-        }
-    }
+        where TEntity : class => (await CommandBus.Send(request)).AsHttpResult();
 }
 
