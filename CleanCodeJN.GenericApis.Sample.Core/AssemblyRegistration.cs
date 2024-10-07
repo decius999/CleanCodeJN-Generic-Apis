45962eb2a848b1dd06defda8d8fc64306a56b991
Jörg Nieveler <joerg.nieveler@live.de>
2024-10-07 11:12:17 +0200

    Add comments to the AssemblyRegistration Marker classes

--- CleanCodeJN.GenericApis.Sample.Core/AssemblyRegistration.cs
+++ CleanCodeJN.GenericApis.Sample.Core/AssemblyRegistration.cs
@@ -1,4 +1,8 @@
 ﻿namespace CleanCodeJN.GenericApis.Sample.Core;
+
+/// <summary>
+/// Assembly Marker for Dependency Injection. DON´T DELETE IT
+/// </summary>
 public abstract class AssemblyRegistration
 {
 }
--- CleanCodeJN.GenericApis.Sample.Domain/AssemblyRegistration.cs
+++ CleanCodeJN.GenericApis.Sample.Domain/AssemblyRegistration.cs
@@ -1,4 +1,8 @@
 ﻿namespace CleanCodeJN.GenericApis.Sample.Domain;
+
+/// <summary>
+/// Assembly Marker for Dependency Injection. DON´T DELETE IT
+/// </summary>
 public abstract class AssemblyRegistration
 {
 }
