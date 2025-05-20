1eef1d4a9299e62d5b4167a4abd77fdcc2fb7325
Jörg Nieveler <joerg.nieveler@live.de>
2025-05-20 21:27:16 +0200

    New Version: 4.1.7 and 3.0.4: Added IfRequest and IfBreakRequest to ICommandExecutionContext.

--- CleanCodeJN.GenericApis.Abstractions/Contracts/ICommandExecutionContext.cs
+++ CleanCodeJN.GenericApis.Abstractions/Contracts/ICommandExecutionContext.cs
@@ -104,4 +104,126 @@ public interface ICommandExecutionContext
     /// <param name="continueOnCheckError">True: Continue executing. False: Stop executing of blocks on errors.</param>
     /// <returns>ICommandExecutionContext for pipelining more blocks.</returns>
     ICommandExecutionContext WithRequest(Func<IRequest<Response>> requestBuilder, string blockName = null, Func<bool> checkBeforeExecution = null, Func<Response, bool> checkAfterExecution = null, bool? continueOnCheckError = false);
+
+    /// <summary>
+    /// Executes a request conditionally based on the provided predicates.
+    /// </summary>
+    /// <remarks>This method allows conditional execution of a request based on pre- and post-execution
+    /// predicates.  If the <paramref name="ifBeforePredicate"/> returns <see langword="false"/>, the request is not
+    /// executed.  If the <paramref name="ifAfterPredicate"/> is provided, it is evaluated after the request execution
+    /// to validate the result.</remarks>
+    /// <typeparam name="T">The type of the entity returned by the request.</typeparam>
+    /// <param name="requestBuilder">A function that builds the request to be executed. Must not be <see langword="null"/>.</param>
+    /// <param name="ifBeforePredicate">An optional predicate that determines whether the request should be executed.  If <see langword="null"/>, the
+    /// request is always executed.</param>
+    /// <param name="ifAfterPredicate">An optional predicate that determines whether the result of the request satisfies a condition.  If <see
+    /// langword="null"/>, the result is not checked after execution.</param>
+    /// <returns>The <see cref="ICommandExecutionContext"/> representing the execution context of the request.</returns>
+    ICommandExecutionContext IfRequest<T>(
+       Func<IRequest<BaseResponse<T>>> requestBuilder,
+       Func<bool> ifBeforePredicate = null,
+       Func<BaseResponse<T>, bool> ifAfterPredicate = null) where T : class =>
+           WithRequest(requestBuilder, checkAfterExecution: ifAfterPredicate, checkBeforeExecution: ifBeforePredicate, continueOnCheckError: true);
+
+    /// <summary>
+    /// Executes a request conditionally based on specified predicates before and after execution. If false, break the whole execution.
+    /// </summary>
+    /// <typeparam name="T">The type of the response data returned by the request.</typeparam>
+    /// <param name="requestBuilder">A function that builds the request to be executed.</param>
+    /// <param name="ifBeforePredicate">An optional predicate to evaluate before executing the request.  If the predicate returns <see
+    /// langword="false"/>, the request will not be executed.</param>
+    /// <param name="ifAfterPredicate">An optional predicate to evaluate after executing the request.  If the predicate returns <see
+    /// langword="false"/>, the execution context will not proceed further.</param>
+    /// <returns>An <see cref="ICommandExecutionContext"/> representing the execution context of the request.</returns>
+    ICommandExecutionContext IfBreakRequest<T>(
+      Func<IRequest<BaseResponse<T>>> requestBuilder,
+      Func<bool> ifBeforePredicate = null,
+      Func<BaseResponse<T>, bool> ifAfterPredicate = null) where T : class =>
+          WithRequest(requestBuilder, checkAfterExecution: ifAfterPredicate, checkBeforeExecution: ifBeforePredicate, continueOnCheckError: false);
+
+    /// <summary>
+    /// Executes a request conditionally based on the provided predicates.
+    /// </summary>
+    /// <remarks>This method is designed to provide conditional execution of a request, allowing both
+    /// pre-execution and post-execution checks through the provided predicates. The request will only proceed if the
+    /// <paramref name="ifBeforePredicate"/> (if provided) evaluates to <see langword="true"/>. After execution, the
+    /// <paramref name="ifAfterPredicate"/> (if provided) can be used to validate the response.</remarks>
+    /// <typeparam name="T">The type of the entity contained in the request's response.</typeparam>
+    /// <param name="requestBuilder">A function that builds the request to be executed. The function must return an <see cref="IRequest{TResponse}"/>
+    /// where the response is of type <see cref="BaseListResponse{TEntity}"/>.</param>
+    /// <param name="ifBeforePredicate">An optional predicate that determines whether the request should be executed. If the predicate returns <see
+    /// langword="false"/>, the request will not be executed. Defaults to <see langword="null"/>, which means the
+    /// request will always be executed unless another condition prevents it.</param>
+    /// <param name="ifAfterPredicate">An optional predicate that evaluates the response after the request is executed. If the predicate returns <see
+    /// langword="false"/>, subsequent operations may be affected based on the implementation. Defaults to <see
+    /// langword="null"/>, which means no post-execution check is performed.</param>
+    /// <returns>An <see cref="ICommandExecutionContext"/> that represents the execution context of the command, allowing further
+    /// chaining or inspection of the operation.</returns>
+    ICommandExecutionContext IfRequest<T>(
+        Func<IRequest<BaseListResponse<T>>> requestBuilder,
+        Func<bool> ifBeforePredicate = null,
+        Func<BaseListResponse<T>, bool> ifAfterPredicate = null) where T : class =>
+            WithRequest(requestBuilder, checkAfterExecution: ifAfterPredicate, checkBeforeExecution: ifBeforePredicate, continueOnCheckError: true);
+
+    /// <summary>
+    /// Executes a request conditionally based on specified predicates. If false, break the whole execution.
+    /// </summary>
+    /// <remarks>This method allows conditional execution of a request based on pre- and post-execution
+    /// predicates. The <paramref name="ifBeforePredicate"/> is evaluated before the request is executed, and the
+    /// <paramref name="ifAfterPredicate"/> is evaluated after the request completes. If either predicate indicates a
+    /// break condition, the execution context will reflect this.</remarks>
+    /// <typeparam name="T">The type of the response data contained in the request.</typeparam>
+    /// <param name="requestBuilder">A function that builds the request to be executed. Must not be <see langword="null"/>.</param>
+    /// <param name="ifBeforePredicate">An optional predicate to evaluate before executing the request. If the predicate returns <see
+    /// langword="false"/>, the request will not be executed.</param>
+    /// <param name="ifAfterPredicate">An optional predicate to evaluate after executing the request. If the predicate returns <see langword="false"/>,
+    /// the execution context will indicate a break condition.</param>
+    /// <returns>An <see cref="ICommandExecutionContext"/> representing the execution context of the request.</returns>
+    ICommandExecutionContext IfBreakRequest<T>(
+       Func<IRequest<BaseListResponse<T>>> requestBuilder,
+       Func<bool> ifBeforePredicate = null,
+       Func<BaseListResponse<T>, bool> ifAfterPredicate = null) where T : class =>
+           WithRequest(requestBuilder, checkAfterExecution: ifAfterPredicate, checkBeforeExecution: ifBeforePredicate, continueOnCheckError: false);
+
+    /// <summary>
+    /// Executes a request conditionally based on the specified predicates.
+    /// </summary>
+    /// <remarks>This method allows conditional execution of a request by evaluating the provided predicates. 
+    /// The <paramref name="ifBeforePredicate"/> is evaluated before the request is sent, and the  <paramref
+    /// name="ifAfterPredicate"/> is evaluated after the request is executed. If either predicate  fails, the execution
+    /// context will handle the error and continue processing.</remarks>
+    /// <typeparam name="T">The type of the entity associated with the request. Must be a reference type.</typeparam>
+    /// <param name="requestBuilder">A function that builds the request to be executed. This function must return an <see cref="IRequest{Response}"/>
+    /// object.</param>
+    /// <param name="ifBeforePredicate">An optional predicate that determines whether the request should be executed before it is sent.  If not
+    /// provided, the request will always be executed.</param>
+    /// <param name="ifAfterPredicate">An optional predicate that determines whether the request should be considered successful after execution, 
+    /// based on the <see cref="Response"/> returned. If not provided, the request will always be considered successful.</param>
+    /// <returns>An <see cref="ICommandExecutionContext"/> that represents the context of the executed command,  allowing further
+    /// chaining or inspection of the execution.</returns>
+    ICommandExecutionContext IfRequest<T>(
+      Func<IRequest<Response>> requestBuilder,
+      Func<bool> ifBeforePredicate = null,
+      Func<Response, bool> ifAfterPredicate = null) where T : class =>
+          WithRequest(requestBuilder, checkAfterExecution: ifAfterPredicate, checkBeforeExecution: ifBeforePredicate, continueOnCheckError: true);
+
+    /// <summary>
+    /// Executes a request conditionally based on specified predicates. If false, break the whole execution.
+    /// </summary>
+    /// <remarks>This method allows conditional execution of a request based on predicates evaluated before
+    /// and after the request execution. If either predicate is not satisfied, the execution context will reflect the
+    /// failure state.</remarks>
+    /// <typeparam name="T">The type of the context or data associated with the request. Must be a reference type.</typeparam>
+    /// <param name="requestBuilder">A function that builds the request to be executed. This function must return an <see cref="IRequest{Response}"/>
+    /// instance.</param>
+    /// <param name="ifBeforePredicate">An optional predicate that is evaluated before the request is executed. If the predicate returns <see
+    /// langword="false"/>, the request will not be executed. Defaults to <see langword="null"/>.</param>
+    /// <param name="ifAfterPredicate">An optional predicate that is evaluated after the request is executed. If the predicate returns <see
+    /// langword="false"/>, the execution context will indicate a failure. Defaults to <see langword="null"/>.</param>
+    /// <returns>An <see cref="ICommandExecutionContext"/> representing the result of the conditional request execution.</returns>
+    ICommandExecutionContext IfBreakRequest<T>(
+      Func<IRequest<Response>> requestBuilder,
+      Func<bool> ifBeforePredicate = null,
+      Func<Response, bool> ifAfterPredicate = null) where T : class =>
+          WithRequest(requestBuilder, checkAfterExecution: ifAfterPredicate, checkBeforeExecution: ifBeforePredicate, continueOnCheckError: false);
 }
