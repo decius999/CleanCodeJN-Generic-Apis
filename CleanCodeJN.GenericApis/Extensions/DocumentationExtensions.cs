a2eb898639599f415b26334e24da41380137e60a
Jörg Nieveler <joerg.nieveler@live.de>
2025-10-19 12:31:48 +0200

    API Integration + Frontend improvements

--- CleanCodeJN.GenericApis/Extensions/DocumentationExtensions.cs
+++ CleanCodeJN.GenericApis/Extensions/DocumentationExtensions.cs
@@ -21,6 +21,45 @@ public static class DocumentationExtensions
             await context.Response.WriteAsync(html);
         });
 
+        app.MapGet("/api/docs", () => Results.Json(new
+        {
+            commands = new[] {
+                new {
+                    name = "SubmissionProcessingEventIntegrationCommand1",
+                    @namespace = "OneApi.Business.CustomMailMessageCommands",
+                    summary = "Steuert den kompletten Lifecycle eines Submission Processing Events.",
+                    steps = new[]
+                        {
+                            new { title = "SetSubmissionStateOrInterruptRequest (PROCESSING_STARTED)", description = "Setzt Submission Status auf PROCESSING_STARTED." },
+                            new { title = "SubmissionGetByIdRequest", description = "Lädt Submission anhand ID." },
+                            new { title = "SendSubmissionSendEvent", description = "Sendet Completion Event." }
+                        }
+                    },
+                 new {
+                    name = "SubmissionProcessingEventIntegrationCommand2",
+                    @namespace = "OneApi.Business.CustomMailMessageCommands",
+                    summary = "Steuert den kompletten Lifecycle eines Submission Processing Events.",
+                    steps = new[]
+                        {
+                            new { title = "SetSubmissionStateOrInterruptRequest (PROCESSING_STARTED)", description = "Setzt Submission Status auf PROCESSING_STARTED." },
+                            new { title = "SubmissionGetByIdRequest", description = "Lädt Submission anhand ID." },
+                            new { title = "SendSubmissionSendEvent", description = "Sendet Completion Event." }
+                        }
+                    },
+                  new {
+                    name = "SubmissionProcessingEventIntegrationCommand3",
+                    @namespace = "OneApi.Business.CustomMailMessageCommands",
+                    summary = "Steuert den kompletten Lifecycle eines Submission Processing Events.",
+                    steps = new[]
+                        {
+                            new { title = "SetSubmissionStateOrInterruptRequest (PROCESSING_STARTED)", description = "Setzt Submission Status auf PROCESSING_STARTED." },
+                            new { title = "SubmissionGetByIdRequest", description = "Lädt Submission anhand ID." },
+                            new { title = "SendSubmissionSendEvent", description = "Sendet Completion Event." }
+                        }
+                    },
+                }
+        }));
+
         return app;
     }
 }
