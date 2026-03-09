d48e188f8e97c71f162f283ccca816a7f2bfcdb0
Jörg Nieveler <joerg.nieveler@live.de>
2026-03-09 17:41:51 +0100

    No filtering or selection in minimal api

--- CleanCodeJN.GenericApis.Sample/Apis/CustomersV1Api.cs
+++ CleanCodeJN.GenericApis.Sample/Apis/CustomersV1Api.cs
@@ -20,9 +20,7 @@ public class CustomersV1Api : IApi
         app => app.MapGet<Customer, CustomerGetDto, int>(
             Route,
             Tags,
-            where: x => x.Name.StartsWith("Customer"),
             includes: [x => x.Invoices],
-            //select: x => new Customer { Id = x.Id, Name = x.Name },
             ignoreQueryFilters: true)
                 .WithSummary("Get all customers including invoices")
                 .WithDescription("Returns a list of all customers which starts with Customer in their Name including their invoices."),
